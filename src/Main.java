ecc8cbddb0beda5aa6a8773bce889f82c081c246
Ferius057 <60037873+Ferius057@users.noreply.github.com>
2020-02-26 17:53:41 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        SimpleGUI app = new SimpleGUI();
+        app.setVisible(true);
+        SimpleGUI1 app1 = new SimpleGUI1();
+        app1.setVisible(true);
+    }
+}

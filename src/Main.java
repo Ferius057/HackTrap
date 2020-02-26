ab39e663175bf27c649f37d1216070e12385aba7
Ferius057 <60037873+Ferius057@users.noreply.github.com>
2020-02-26 17:54:25 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
-package com.company;
-
 public class Main {
 
     public static void main(String[] args) {
-        SimpleGUI app = new SimpleGUI();
-        app.setVisible(true);
-        SimpleGUI1 app1 = new SimpleGUI1();
-        app1.setVisible(true);
+        //without the code
     }
 }
